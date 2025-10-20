6b7c86db125eef323a8d3a33dd92ce844b500066
Caleb Lamptey <alizwel008@gmail.com>
2025-10-20 15:08:49 +0000

    edited course card

--- frontend/src/components/CourseCard.jsx
+++ frontend/src/components/CourseCard.jsx
@@ -1,11 +1,56 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { Trophy, Star } from "lucide-react";
+import {
+  Trophy,
+  Star,
+  RectangleGoggles,
+  Box,
+  CodeXml,
+  Drone,
+  Handshake,
+} from "lucide-react";
+import { FaPython } from "react-icons/fa";
 import { IoPerson } from "react-icons/io5";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const CourseCard = ({ course }) => {
   return (
-    <div className="bg-sidebar border border-border flex flex-col justify-between rounded-xl p-3 hover:scale-[1.01] duration-300">
+    <div className="bg-sidebar border border-border flex flex-col justify-between rounded-xl p-3 hover:scale-[1.01] duration-300 relative">
+      <Tooltip>
+        <div className="absolute top-6 right-6 w-8 aspect-square flex justify-center items-center rounded-xl bg-accent text-foreground">
+          <TooltipTrigger>
+            {course.topic === "Drone Piloting" && <Drone size={13} />}
+          </TooltipTrigger>
+          <TooltipTrigger>
+            {course.topic === "VR Development" && (
+              <RectangleGoggles size={13} />
+            )}
+          </TooltipTrigger>
+
+          <TooltipTrigger>
+            {course.topic === "Web Development" && <CodeXml size={13} />}
+          </TooltipTrigger>
+
+          <TooltipTrigger>
+            {course.topic === "Python Programming" && <FaPython size={13} />}
+          </TooltipTrigger>
+
+          <TooltipTrigger>
+            {course.topic === "Entrepreneurship" && <Handshake size={13} />}
+          </TooltipTrigger>
+
+          <TooltipTrigger>
+            {course.topic === "3D Modeling" && <Box size={13} />}
+          </TooltipTrigger>
+        </div>
+        <TooltipContent>
+          <p>{course.topic}</p>
+        </TooltipContent>
+      </Tooltip>
       <div>
         <img
           src={course.thumbnail}
@@ -40,7 +85,7 @@ const CourseCard = ({ course }) => {
         </div>
       </div>
 
-      <div className="flex items-center mt-2 justify-between">
+      <div className="flex items-center mt-5 justify-between">
         <p className="text-lg font-medium ">{course.price}</p>
         <Button className="cursor-pointer" variant="default">
           Start
